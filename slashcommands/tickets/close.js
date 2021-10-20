fc4cce02bd253c9b27842c3c1995fe913fab965e
xxproxxtim <85392782+xxproxxtim@users.noreply.github.com>
2021-10-20 14:26:01 +0200

    Update close.js

--- slashcommands/tickets/close.js
+++ slashcommands/tickets/close.js
@@ -6,6 +6,9 @@ module.exports.run = async (client, inter) => {
 
     const closeinfo = inter.options.getString('close-text');
 
+    const channel = client.channels.cache.find(c => c.id === "861221128373927957")
+    if(channel === undefined) return
+
     if(db.get(`ticketChannel_${inter.channelId}`) != true){
         return await inter.reply(`Dit commando kan je enkel in een ticket kanaal doen!`)
     }
@@ -13,12 +16,14 @@ module.exports.run = async (client, inter) => {
     await inter.reply(`Deze ticket is succesvol gesloten en zal zo worden verwijderd!`)
 
     const thanksEmbed = new Discord.MessageEmbed()
-            .setTimestamp()
-            .setTitle('Gesloten')
-            .setDescription('Ticket gesloten!')
-            .addField('Toevoeging', closeinfo)
-            .setColor('RANDOM')
-    inter.user.send({ embeds: [thanksEmbed] })
+        .setTimestamp()
+        .setTitle('Gesloten')
+        .setDescription(`Ticket gesloten door ${inter.user.username}`)
+        .addField('Toevoeging', closeinfo)
+        .addField(`Naam`, inter.channelId)
+        .setColor('RANDOM')
+        .setFooter(`Gesloten door ${inter.user.username}`)
+    channel.send({ embeds: [thanksEmbed] })
     
     db.delete(`ticket_${inter.user.id}`)
     db.delete(`ticketChannel_${inter.channelId}`)
